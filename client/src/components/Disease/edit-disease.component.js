d7ac5dd0db791b11c7ede929901a01374be7d94b
Fan Lu <fl427@cornell.edu>
2020-04-05 23:38:22 -0400

    Diseases List Create Edit Delete

--- client/src/components/Disease/edit-disease.component.js
+++ client/src/components/Disease/edit-disease.component.js
@@ -1,12 +1,143 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class EditDiseases extends Component {
-    render() {
-        return (
+  constructor(props) {
+    super(props);
+
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      name: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/diseases/" + this.props.match.params.id)
+      .then((response) => {
+        this.setState({
+          name: response.data.name,
+          description: response.data.description,
+          duration: response.data.duration,
+          date: new Date(response.data.date),
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  onChangeName(e) {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value,
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value,
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const disease = {
+      name: this.state.name,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date,
+    };
+
+    console.log(disease);
+
+    axios
+      .post(
+        "http://localhost:5000/diseases/update/" + this.props.match.params.id,
+        disease
+      )
+      .then((res) => {
+        console.log(res.data);
+        window.location = "/";
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Edit Exercise Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Name: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.name}
+              onChange={this.onChangeName}
+            />
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Duration (in minutes): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
             <div>
-                <p>You are on the Diseases Edit Component!</p>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
             </div>
-        )
-    }
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Edit Disease Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
--- client/src/components/Home/HeartRate.js
+++ client/src/components/Home/HeartRate.js
@@ -5,13 +5,13 @@ class HeartRate extends React.Component {
     render() {
         return (
             <div>
-                <div class="card" style={{width: '18rem'}}>
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <Link to="/" class="card-link">Card link</Link>
-                        <Link to="/" class="card-link">Another link</Link>
+                <div className="card" style={{width: '18rem'}}>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <Link to="/" className="card-link">Card link</Link>
+                        <Link to="/" className="card-link">Another link</Link>
                     </div>
                 </div>
             </div>
