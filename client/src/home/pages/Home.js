476eaf8ab8f5011d642db5a41ce97a680cec705a
Fan Lu <fl427@cornell.edu>
2020-04-25 16:27:31 -0400

    Get Image from Database to fill Scenerios

--- client/src/home/pages/Home.js
+++ client/src/home/pages/Home.js
@@ -1,61 +1,87 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import {Link} from "react-router-dom";
 
 import Logs from "../components/Logs";
-import Scenario from "../components/Scenario";
+import ScenarioItem from "../components/ScenarioItem";
 import Monitor from "../components/Monitor";
 import HeartRate from "../components/HeartRate";
 
-import '../components/Logs.css'
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div className="container-fluid">
+import '../components/Logs.css'
+import ScenerioList from "../components/ScenerioList";
+
+const Home = props => {
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const [loadedDogs, setLoadedDogs] = useState();
+
+    useEffect(() => {
+        const fetchDogs = async () => {
+            try {
+                const responseData = await sendRequest(
+                    'http://localhost:5000/api/dogs'
+                );
+
+                setLoadedDogs(responseData.dogs);
+            } catch (err) {
+                console.log(err.message || "Something went wrong, please try again.")
+            }
+        };
+        fetchDogs();
+    }, [sendRequest]);
+
+    console.log(loadedDogs)
+
+    return (
+        <div className="container-fluid">
+            {/*HeartRate*/}
+            {/*Monitor 01*/}
+            {/*Monitor 02*/}
+            {/*ScenarioItem*/}
+            <div className="row">
                 {/*HeartRate*/}
                 {/*Monitor 01*/}
                 {/*Monitor 02*/}
-                {/*Scenario*/}
-                <div className="row">
+                <div className="col-md-7">
                     {/*HeartRate*/}
-                    {/*Monitor 01*/}
-                    {/*Monitor 02*/}
-                    <div className="col-md-7">
-                        {/*HeartRate*/}
-                        <div id="panel0" className="row">
-                            <h2>HeartRate</h2>:<h5>The starting state of the menu will appear collapsed on smaller screens, and will appear
-                                non-collapsed on larger screens. When toggled using the button below, the menu will
-                                change.</h5>
-                        </div>
-                        {/*Monitor 01*/}
-                        <div id="panel1" className="row">
-                            <h2>Monitor 01</h2>:<h5>The starting state of the menu will appear collapsed on smaller screens, and will appear
-                            non-collapsed on larger screens. When toggled using the button below, the menu will
-                            change.</h5>
-                        </div>
-                        {/*Monitor 02*/}
-                        <div id="panel2" className="row">
-                            <h2>Monitor 02</h2>:<h5>The starting state of the menu will appear collapsed on smaller screens, and will appear
-                            non-collapsed on larger screens. When toggled using the button below, the menu will
-                            change.</h5>
-                        </div>
+                    <div id="panel0" className="row">
+                        <h2>HeartRate</h2>:<h5>The starting state of the menu will appear collapsed on smaller screens,
+                        and will appear
+                        non-collapsed on larger screens. When toggled using the button below, the menu will
+                        change.</h5>
                     </div>
-
-                    {/*Scenario*/}
-                    <div className="col-md-4">
-                        <Scenario />
+                    {/*Monitor 01*/}
+                    <div id="panel1" className="row">
+                        <h2>Monitor 01</h2>:<h5>The starting state of the menu will appear collapsed on smaller screens,
+                        and will appear
+                        non-collapsed on larger screens. When toggled using the button below, the menu will
+                        change.</h5>
+                    </div>
+                    {/*Monitor 02*/}
+                    <div id="panel2" className="row">
+                        <h2>Monitor 02</h2>:<h5>The starting state of the menu will appear collapsed on smaller screens,
+                        and will appear
+                        non-collapsed on larger screens. When toggled using the button below, the menu will
+                        change.</h5>
                     </div>
                 </div>
 
-                {/*Logs*/}
-                <div className="row">
-                    <div className="col-md-11 panel-warning">
-                        <Logs />
-                    </div>
+                {/*ScenarioItem*/}
+                <div className="col-md-4">
+                    {loadedDogs && (
+                        <ScenerioList items={loadedDogs} />
+                    )}
                 </div>
             </div>
-        );
-    }
+
+            {/*Logs*/}
+            <div className="row">
+                <div className="col-md-11 panel-warning">
+                    <Logs/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
