0a44e0ebf178bb3a9b8d80520e7e016e38cc6dc0
Fan Lu <fl427@cornell.edu>
2020-04-26 23:52:27 -0400

    modify media

--- client/src/media/pages/Media.js
+++ client/src/media/pages/Media.js
@@ -7,6 +7,12 @@ const Media = props => {
     return (
         <React.Fragment>
             <h1>Media - MRI - Blood Work - Chest XRay</h1>
+            {/*<iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'*/}
+            {/*        frameBorder='0'*/}
+            {/*        allow='autoplay; encrypted-media'*/}
+            {/*        allowFullScreen*/}
+            {/*        title='video'*/}
+            {/*/>*/}
 
         </React.Fragment>
     );
