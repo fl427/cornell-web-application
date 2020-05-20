be8640a14f58fb526ee93d15cf9e95e93296c062
Siyu Liu <55250584+siyuliu-1213@users.noreply.github.com>
2020-05-20 04:01:07 -0400

    build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.301731a65c7682877556ed02eef9bda5.js"
+  "/precache-manifest.0b935e7f61f96fa23887b421a7230e25.js"
 );
 
 self.addEventListener('message', (event) => {
