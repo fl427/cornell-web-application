fef1ea471a12b70bb0f24f634a0997b73d02f8fa
Fan Lu <fl427@cornell.edu>
2020-05-20 15:56:25 -0400

    change interval in backend

---
+++ server/models/pulse/rightDorsalPulse.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const rightDorsalPulseSchema = mongoose.Schema({
+    content: [{type: Number}]
+}, {
+    timestamps: { createdAt: true, updatedAt: false }
+});
+
+module.exports = mongoose.model("RightDorsalPulse", rightDorsalPulse);
