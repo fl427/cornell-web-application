bde6795a9b6ae5275318d01532c434da2e064ba5
Fan Lu <fl427@cornell.edu>
2020-05-21 12:35:59 -0400

    deploy url

--- client/src/URLs.js
+++ client/src/URLs.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://localhost:5000'
+const baseURL = 'https://cornell-vet.herokuapp.com'
 
 module.exports = {
     baseURL: baseURL
--- client/src/request/fetch.js
+++ client/src/request/fetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let $axios = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://cornell-vet.herokuapp.com",
     timeout: 5000,
     headers: {"Content-Type": "application/json"}
 });
