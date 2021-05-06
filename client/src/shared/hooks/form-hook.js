3bdf694096b86a96d8a3c62c2b32fa06f76082ca
Peihan Gao <47938993+legendgp7@users.noreply.github.com>
2021-05-06 19:46:24 -0400

    Update form-hook.js

--- client/src/shared/hooks/form-hook.js
+++ client/src/shared/hooks/form-hook.js
@@ -32,6 +32,7 @@ const formReducer = (state, action) => {
     }
 };
 
+
 export const useForm = (initialInputs, initialFormValidity) => {
     const [formState, dispatch] = useReducer(formReducer, {
         inputs: initialInputs,
